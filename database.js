865f15aa38fe2dfef7ebb65871a040b3186e8114
mwidurska <57037576+mwidurska@users.noreply.github.com>
2021-12-31 11:09:04 +0100

    Update database.js

--- database.js
+++ database.js
@@ -15,7 +15,6 @@ var project1 = {
             "type": "Point",
             "coordinates": [23.15003, 53.10611]
             }
-        
 };
 
 var project2 = {
@@ -35,7 +34,6 @@ var project2 = {
         "type": "Point",
         "coordinates": [22.92551, 54.09664]
         }
-    
 };
 var project3 = {
     
@@ -54,7 +52,6 @@ var project3 = {
         "type": "Point",
         "coordinates": [22.92402, 54.09662]
         }
-    
 };
 
 var project4 = {
@@ -86,7 +83,6 @@ var project4 = {
                     },
                     ]
                 }
-    
 };
 var project5 = {
     
@@ -105,8 +101,8 @@ var project5 = {
         "type": "Point",
         "coordinates": [23.13953, 53.04486]
         }
-    
 };
+
 var project6 = {
     
     "type": "Feature",
@@ -152,128 +148,57 @@ var project6 = {
     
 };
 
-var hajnowkaCity = {
-    "type": "FeatureCollection",
-    "features": [
-        {
-            "type": "Feature",
-            "properties": {
-                "name": "Miasto Hajnówka",
-                "PRtype": "LPR",
-                "PRLegalBasis": "Wytyczne w zakresie rewitalizacji w programach operacyjnych na lata 2014-2020 ",
-                "PRname": "Lokalny Program Rewitalizacji Miasta Hajnówka",
-                "description": "opis programu rewitalizacji",
-                "link": "http://bip.hajnowka.pl/article/uchwala-nr-xxxiii-ukosnik-223-ukosnik-17-rady-miasta-hajnowka-z-dnia-20-listopada-2017r",
-            },
-            "geometry": {
-                "type": "Polygon",
-                "coordinates": [[
-                   [23.5290243,52.7069763],[23.5290581,52.7074235],[23.5294547,52.7113653],[23.5297024,52.7134423],[23.5300416,52.7167706],[23.5305361,52.7204026],[23.5306813,52.7216151],[23.531493,52.7287454],[23.531736,52.7313745],[23.5427727,52.7294809],[23.545458,52.7290241],[23.5450194,52.7325122],[23.5440972,52.7384286],[23.5440467,52.7387183],[23.5438231,52.7403095],[23.5435345,52.7421823],[23.5432703,52.7442079],[23.545149,52.7472311],[23.5463432,52.749168],[23.5481378,52.7520205],[23.5502805,52.7554726],[23.5542689,52.7618086],[23.5588418,52.7610721],[23.5595086,52.7609707],[23.5624591,52.7604874],[23.5653156,52.7600161],[23.5724064,52.7588318],[23.5727465,52.7598675],[23.5757349,52.759145],[23.5786662,52.7582205],[23.5794894,52.7599144],[23.5801952,52.7613916],[23.5798989,52.7614435],[23.5800327,52.7617329],[23.5800712,52.7618157],[23.5803787,52.7617585],[23.5809136,52.7614753],[23.5816591,52.7612368],[23.5816305,52.7611761],[23.5854355,52.7599898],[23.5857526,52.7598985],[23.588115,52.7591673],[23.5923748,52.757833],[23.593383,52.755835],[23.5977266,52.7545008],[23.5978044,52.7545103],[23.597916,52.7545569],[23.5979626,52.754575],[23.6003943,52.7521835],[23.6009118,52.752168],[23.6007021,52.7515037],[23.6015136,52.7514729],[23.6052787,52.7509001],[23.6103065,52.7497554],[23.6098708,52.7483199],[23.6101007,52.7482712],[23.6155289,52.7482061],[23.6196216,52.7481214],[23.6235921,52.7480297],[23.6234484,52.7475718],[23.6215408,52.7471031],[23.6211688,52.7470185],[23.6203376,52.7467994],[23.6190277,52.7462727],[23.6135992,52.7437961],[23.612529,52.7432434],[23.6115375,52.7426167],[23.6108442,52.7421],[23.6106557,52.741943],[23.612318,52.7419877],[23.6138465,52.7419849],[23.6137376,52.7418719],[23.6137238,52.7405712],[23.6137469,52.7379199],[23.6138535,52.737767],[23.6138693,52.737643],[23.6136308,52.7374219],[23.6130124,52.7366124],[23.6122266,52.7356375],[23.6116995,52.7350416],[23.6114182,52.7348677],[23.6097357,52.7349896],[23.6094883,52.7349933],[23.6082244,52.7348828],[23.6073809,52.7348501],[23.6069388,52.7348184],[23.6066661,52.7347762],[23.6058207,52.734569],[23.6058357,52.7338284],[23.605904,52.7335548],[23.6058535,52.7333168],[23.605856,52.7323407],[23.6040417,52.7323607],[23.6013278,52.7323348],[23.5992218,52.7323301],[23.5991006,52.7323092],[23.5990021,52.7324152],[23.5987619,52.7323785],[23.5988584,52.7322683],[23.5984058,52.7320964],[23.5974603,52.7321459],[23.5972636,52.7311554],[23.5965567,52.731225],[23.5961851,52.7293543],[23.5960453,52.7289924],[23.5939972,52.7272477],[23.5954246,52.7265793],[23.5953442,52.7265054],[23.5953412,52.7262444],[23.595121,52.7261178],[23.5961558,52.7254767],[23.5963511,52.7243093],[23.5958602,52.7238105],[23.5958414,52.7237833],[23.5965212,52.723638],[23.5966396,52.7234791],[23.5966167,52.7232038],[23.596016,52.7231734],[23.5934183,52.7236387],[23.5928921,52.7237294],[23.5920187,52.7239111],[23.5921179,52.7236713],[23.5923148,52.7227496],[23.5922122,52.7213744],[23.5915251,52.7200618],[23.5896695,52.7205841],[23.5872967,52.7204916],[23.5868317,52.7201422],[23.5853531,52.7192823],[23.5845098,52.7189682],[23.5826909,52.7202965],[23.5834154,52.7207436],[23.5826496,52.7213885],[23.5817439,52.721731],[23.5799259,52.7215971],[23.5792095,52.7217419],[23.5785137,52.7221696],[23.5777749,52.7221598],[23.5760853,52.7231483],[23.575069,52.7224283],[23.5739424,52.721364],[23.5727068,52.7203934],[23.5709303,52.7191511],[23.5691598,52.7178819],[23.5621213,52.7178777],[23.5619089,52.7161664],[23.5594543,52.7150549],[23.5570737,52.7140223],[23.5547595,52.7154755],[23.5518808,52.7141661],[23.5490383,52.7128958],[23.5524033,52.7119436],[23.5541338,52.7112305],[23.552873,52.7100431],[23.5531947,52.7094864],[23.5538616,52.7088517],[23.5544192,52.70848],[23.5560863,52.707352],[23.5547155,52.7069048],[23.5540515,52.7066802],[23.5529647,52.706323],[23.5521461,52.70622],[23.5528,52.7050389],[23.5530805,52.7042161],[23.5517898,52.7036235],[23.5524556,52.7022745],[23.5515411,52.7021604],[23.5506343,52.7041698],[23.550283,52.7050081],[23.5496038,52.7064968],[23.5494905,52.7067333],[23.549377,52.7069963],[23.5491573,52.7074383],[23.5487997,52.7071532],[23.547615,52.7061772],[23.5468595,52.7058189],[23.5462954,52.7054324],[23.5449598,52.7047878],[23.5437617,52.7042934],[23.5428427,52.7037653],[23.5420136,52.7034636],[23.5411891,52.7029248],[23.5401899,52.7023718],[23.5382161,52.7036002],[23.5322375,52.7074164],[23.5320137,52.707375],[23.5320709,52.7072509],[23.5309881,52.7071512],[23.5309215,52.7072368],[23.5290243,52.7069763]
-
-
-                ]]
-            }
-
-
-        }, {   
-	"type": "Feature",
-	"properties": {
-		"title": "Modernizacja kompleksu  sportowo – rekreacyjnego OSiR  – nadanie nowych funkcji dla obiektu zdegradowanego zabezpieczających  potrzeby  społeczne i gospodarcze osób zamieszkałych na terenie miasta Hajnówka, w tym w obszarze zdegradowanym.",
-        "sphere": "spatial",
-        "description": "Projekt składał się z kilku etapów. Miasto zmodernizowało stadion, zbudowało dwa baseny w miejscu starych, które już nie nadawały się do użytku, a także zagospodarowało teren Ośrodka Sportu i Rekreacji.</p><p>Stadion w Hajnówce przeszedł gruntowną modernizację. Amatorzy sportu mogą korzystać z boiska do piłki nożnej, kortów tenisowych oraz boiska do siatkówki. Znajduje się tu też stadion lekkoatletyczny i bieżnia.</p><p>Nowe baseny - mały i duży, będą służyć zarówno dzieciom jak i dorosłym. Mieszkańcy mogą z nich korzystać bezpłatnie.",
-        "picture": "https://rpo.wrotapodlasia.pl/resource/image/236/333/11238/16933/0x0.jpg",
-        "pictureAlt": "Stadion lekkoatletyczny na terenie Ośrodka Sportu i Rekreacji w Hajnówce"
-        },
-
-	  "geometry": {
-		"type": "GeometryCollection",
-		"geometries": [
-			{
-			"type": "Point",
-			"coordinates": [23.58077, 52.74712]
-			},
-			{
-			"type": "Polygon",
-			"coordinates": [[
-				[23.5807136, 52.7484916],[23.5820070, 52.7484812],[23.5815788, 52.7452865],
-              [23.5813475, 52.7452097],[23.5793683, 52.7460459],[23.5798831, 52.7468760],
-              [23.5792601, 52.7477009],[23.5807136, 52.7484916],
-				]]
-			},
-			]
-        }
-
-
-}, {   
-	"type": "Feature",
-	"properties": {
-		"title": " Spacery tematyczne z przewodnikiem.",
-        "sphere": "social",
-        "description": "Opis",
-
-        },
-
-    "geometry": {
-        "type": "GeometryCollection",
-		"geometries": [
-			{
-			"type": "Point",
-			"coordinates": [23.58493, 52.74182]
-			},
-			{
-			"type": "MultiLineString",
-        "coordinates": [
-		[[23.5817705, 52.7393209],[23.5818859, 52.7393960],[23.5818952, 52.7394046],[23.5826293, 52.7400819],[23.5831796, 52.7404858],[23.5835909, 52.7407779],[23.5850575, 52.7417321]],
-		[[23.5848163, 52.7419053],[23.5847098, 52.7418656],[23.5845966, 52.7418556],[23.5832167, 52.7419472],[23.5826798, 52.7419760],[23.5822658, 52.7419518],[23.5818397, 52.7418683],[23.5814944, 52.7417866],[23.5814429, 52.7417667],[23.5811684, 52.7416686],[23.5807425, 52.7414607],[23.5806783, 52.7414288],[23.5805573, 52.7413687]],
-          [[23.5856828, 52.7391176],[23.5840318, 52.7400192],[23.5831796, 52.7404858]],
-          [[23.5815750, 52.7449212],[23.5817057, 52.7448135],[23.5818802, 52.7446499],[23.5818802, 52.7446499],[23.5830472, 52.7432143],[23.5830555, 52.7432049],[23.5831324, 52.7431176],[23.5832364, 52.7430088],[23.5834477, 52.7428326],[23.5836534, 52.7426808],[23.5840698, 52.7424074],[23.5844232, 52.7421699],[23.5848163, 52.7419053],[23.5850575, 52.7417321],[23.5852534, 52.7415862],[23.5856442, 52.7413005],[23.5865893, 52.7405829],[23.5866743, 52.7405176],[23.5867977, 52.7404335],[23.5875203, 52.7399136],[23.5876996, 52.7397828],[23.5878465, 52.7396665],[23.5879781, 52.7395387],[23.5881236, 52.7393831],[23.5883085, 52.7391601],[23.5885795, 52.7388198],[23.5886957, 52.7386956]],
-          [[23.5822592, 52.7390611],[23.5828323, 52.7389855],[23.5832155, 52.7389388],[23.5835311, 52.7388990],[23.5839039, 52.7388511],[23.5845619, 52.7387666],[23.5848770, 52.7387288],[23.5855715, 52.7386456],[23.5858058, 52.7386155],[23.5861235, 52.7385747],[23.5870899, 52.7384507],[23.5875445, 52.7383923],[23.5881498, 52.7383135],[23.5900170, 52.7380788],[23.5902376, 52.7380503],[23.5905577, 52.7380036],[23.5908515, 52.7379441],[23.5911248, 52.7378710],[23.5913625, 52.7377890],[23.5915589, 52.7377071]]
-		]
-			},
-			]
-        }
-
-}, {   
-	"type": "Feature",
-	"properties": {
-		"title": " Zagospodarowanie terenu wokół Hajnowskiego Domu Kultury w celu nadania funkcji kulturalno–społecznej.",
-        "sphere": "spatial",
-        "description": "Przy Hajnowskim Domu Kultury powstała Rodzinna Strefa Zabaw. To supermiejsce dla małych i dużych. Dla dzieci: huśtawki, karuzela, bujaki, dla dorosłych: wygodne leżaki i dostęp do Wi-Fi.</p><p>Rodzinna Strefa Zabaw wyposażona jest m.in. w huśtawki, karuzelę, bujaki, różne urządzenia sprawnościowe. Jest tam również wiata z systemem zasilania solarnego (z możliwością ładowania urządzeń mobilnych). Rodzice, czekając na dzieci, mogą odpocząć na wygodnych wyprofilowanych leżakach, przy których znajdują się parkowe stoliki.</p><p>Inwestycja już w dniu otwarcia podbiła serca hajnowian, zwłaszcza tych najmłodszych.",
-        "picture": "https://rpo.wrotapodlasia.pl/resource/image/236/333/8679/13713/0x0.jpg",
-         "pictureAlt": "Dzieci bawią się na siatkowej konstrukcji",
-        },
-
-	  "geometry": {
-		"type": "GeometryCollection",
-		"geometries": [
-			{
-			"type": "Point",
-			"coordinates": [23.57899, 52.74726]
-			},
-			{
-			"type": "Polygon",
-			"coordinates": [[
-				[23.57845, 52.74711],[23.57881, 52.74697],[23.57967, 52.74706],[23.57922, 52.74772],[23.57887, 52.74752]
-				]]
-			},
-			]
-        }
-
-
-},{   
-	"type": "Feature",
-	"properties": {
-		"title": "EKO-Hajnówka – rozbudowa sieci dystrybucji ciepła na terenie miasta wraz z wymianą źródeł ciepła w gospodarstwach domowych oraz wymianą oświetlenia na energooszczędne.",
-        "sphere": "environmental",
-        "description": "W ramach projektu zmodernizowano piece w 37 prywatnych budynkach mieszkalnych, wymieniono 2011 punktów oświetlenia ulicznego na energooszczędne, a także zakupiono stację do pomiaru czystości powietrza.</p><p>Aktualny stan jakości powietrza mieszkańcy mogą sprawdzić na stronie internetowej miasta.",
-         
+var project7 = {
+    
+    "type": "Feature",
+    "properties": {
+        "title": 'Utworzenie miejsc opieki nad dziećmi do lat 3 w Żłobku "Maluszek"',
+        "beneficiary": "Danwood S.A.",
+        "sphere": "2",
+        "description": "Firma Danwood S.A. stworzyła przyzakładowy żłobek. Dotacja została przeznaczona na doposażenie placówki, budowę placu zabaw oraz stworzenie miejsc opieki dla dzieci do lat 3. Żłobek zapewnia profesjonalną opiekę i edukację dla 24 dzieci pracowników firmy.</p><p>Wyposażenie żłobka i plac zabaw zostały wykonane zgodnie z z zasadami uniwersalnego projektowania. Maluszki mogą korzystać z przestronnej i atrakcyjnej sali zabaw, kuchni, toaletą, w tym także toaletą dostosowana do potrzeb osób niepełnosprawnych. Żłobek mieści się w białostockiej siedzibie firmy Danwood S.A. przy ulicy Ciołkowskiego.</p><p>Dzięki tej inwestycji firma chce ułatwić swoim pracownikom pogodzenie pracy zawodowej z wychowaniem dzieci, stworzyć dogodne warunki do aktywności zawodowej i równowagi z życiem rodzinnym.",
+        "picture": "https://rpo.wrotapodlasia.pl/resource/image/236/333/8496/13436/0x0.jpg",
+        "pictureAlt": "Wyposażenie wnętrza żłobka – stoliki i sala zabaw",
+        "link": "https://rpo.wrotapodlasia.pl/pl/dowiedz_sie_wiecej_o_programie/poznaj_projekty/przyzakladowy-zlobek-w-danwood-sa.html",
         },
 
     "geometry": {
         "type": "Point",
-        "coordinates": [23.5894431, 52.7374633]
+        "coordinates": [23.16343, 53.10834]
         }
+};
 
-},
-    ]
+var project8 = {
+    "type": "Feature",
+    "properties": {
+        "title": 'ZSHE swoim uczniom na rzecz konkurencyjności podlaskiej gospodarki',
+        "beneficiary": "Miasto Białystok",
+        "sphere": "3",
+        "description": "Zespół Szkół Handlowo-Ekonomicznych w Białymstoku kształci uczniów w zawodach technik ekonomista, technik handlowiec i technik hotelarstwa. Szkoła pozyskała dotację, która umożliwiła uczniom odbycie staży zawodowych u pracodawców. Dzięki stażom podopieczni mogli doświadczyć pracy w danym zawodzie, poszerzyć swoją wiedzę i praktycznie przećwiczyć swoje umiejętności.</p><p>Przed obyciem stażu, uczniowie spotykali się z doradcami zawodowymi, którzy dostosowywali staże do predyspozycji, potrzeb i oczekiwań uczniów zarówno co do miejsca realizacji jak i programu.</p><p>Uczniowie odbyli 381 staży zawodowych u pracodawców, w tym 46 wakacyjnych staży wyjazdowych. Część uczniów miała okazję odbyć w ramach projektu dwa staże. Z tej możliwości skorzystało 60 stażystów. Każdy ze staży trwał 150 godzin i kończył się egzaminem u pracodawcy. Po pomyślnym zdaniu egzaminu stażyści otrzymywali certyfikat oraz stypendium stażowe.</p><p>Celem projektu było również propagowanie wśród stażystów tematyki osiągania celów ekonomicznych z poszanowaniem zasobów środowiska naturalnego i konieczności podejmowania działań na rzecz poprawy jego jakości. 49 staży zrealizowano w firmach z sektora regionalnych inteligentnych specjalizacji.",
+        "picture": "https://rpo.wrotapodlasia.pl/resource/image/236/333/7272/11779/0x0.jpg",
+        "pictureAlt": "Troje uczniów za ladą w restauracji składa serwetki",
+        "link": "https://rpo.wrotapodlasia.pl/pl/dowiedz_sie_wiecej_o_programie/poznaj_projekty/nowoczesne-ksztalcenie-zawodowe-w-zespole-szkol-handlowo-ekonomicznych-w-bialymstoku.html",
+        },
+
+    "geometry": {
+        "type": "Point",
+        "coordinates": [23.13662, 53.11791]
+        }
+};
+
+var project9 = {
+    "type": "Feature",
+    "properties": {
+        "title": 'Łomża - Śniadowo - przebudowa drogi wojewódzkiej Nr 677',
+        "beneficiary": "Województwo Podlaskie",
+        "sphere": "4",
+        "description": "Obwodnica Konarzyc, która jest częścią modernizowanego odcinka drogi wojewódzkiej nr 677, w styczniu 2020 roku została oficjalnie przekazana do użytku. Konarzyce to przyległa do Łomży wieś, która stanowi długi obszar zwartej zabudowy. Z punktu widzenia kierowców, obszar ten znacznie spowalnia jazdę na czterokilometrowym odcinku drogi wojewódzkiej. Z kolei dla jej mieszkańców intensywny ruch samochodowy jest mocno uciążliwy – jest źródłem hałasu, zanieczyszczeń powietrza oraz wiążą się z nim poważne zagrożenia bezpieczeństwa.</p><p>Projekt przewidywał wykonanie wszystkich niezbędnych elementów służących sprawnemu, bezpiecznemu i bardziej komfortowemu poruszaniu się wszystkich uczestników ruchu, wykonanie odpowiedniego systemu odwodnienia oraz przebudowę kolidujących z inwestycją sieci uzbrojenia.W ramach przebudowy drogi wykonano m.in.: budowę ścieżki rowerowej o nawierzchni asfaltowej, budowę nowych i przebudowę istniejących zatok autobusowych, budowę chodników w rejonie zatok autobusowych oraz w terenie zabudowanym, przebudowę i budowę skrzyżowań z drogami gminnymi, a także budowę dróg serwisowych o nawierzchni bitumicznej.",
+        "picture": "https://rpo.wrotapodlasia.pl/resource/image/236/333/9624/15017/0x0.jpg",
+        "pictureAlt": "Samochód ciężarowy jedzie ulicą",
+        "link": "https://rpo.wrotapodlasia.pl/pl/dowiedz_sie_wiecej_o_programie/poznaj_projekty/obwodnica-konarzyc-oficjalnie-otwarta.html",
+        },
+
+    "geometry": {
+        "type": "Point",
+        "coordinates": [23.13662, 53.11791]
+        }
 };
